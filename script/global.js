97665210c41552e747bb4394fbb6bdd699dd3234
Sam Enoka <slwenoka@gmail.com>
2014-09-06 22:53:21 +1000

    created first bar graph

--- script/global.js
+++ script/global.js
@@ -1,1 +1,11 @@
 // D3 Scripting Lain Here
+/* global d3:false */
+
+var dataset = [5, 10, 15, 20, 25];
+
+d3.select('body').selectAll('div')
+	.data(dataset).enter().append('div')
+	.attr('class','bar')
+	.style('height', function(d){
+		return d * 5 + 'px';
+	});
---
+++ script/min/global-min.js
@@ -0,0 +1,1 @@
+var dataset=[5,10,15,20,25];d3.select("body").selectAll("div").data(dataset).enter().append("div").attr("class","bar").style("height",function(t){return 5*t+"px"});

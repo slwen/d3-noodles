715bda867fced716c8df03265053556eb5d8acee
Sam Enoka <slwenoka@gmail.com>
2014-09-15 08:05:28 +1000

    switching back in to div graphs

--- script/global.js
+++ script/global.js
@@ -3,27 +3,30 @@
 
 var dataset = [22, 16, 39, 28, 29, 31, 7, 12, 36];
 
-// d3.select('body').selectAll('div')
-// 	.data(dataset).enter().append('div')
-// 	.attr('class','bar')
-// 	.style('height', function(d){
-// 		return d * 5 + 'px';
-// 	});
+// Bar Chart
+d3.select('body').selectAll('div')
+	.data(dataset).enter().append('div')
+	.attr('class','bar')
+	.style('height', function(d){
+		return d * 5 + 'px';
+	});
 
-var svg = d3.select('body').append('svg');
-var w = 800;
-var h = 450;
 
-svg.attr('width',w);
-svg.attr('height',h);
+// SVG Circle Chart
+// var svg = d3.select('body').append('svg');
+// var w = 800;
+// var h = 450;
 
-var circles = svg.selectAll('circle').data(dataset).enter().append('circle');
+// svg.attr('width',w);
+// svg.attr('height',h);
 
-circles.attr('cx', function(d, i){
-	return (i * 50) + 70;
-})
-.attr('cy', h/2)
-.attr('r', function(d){
-	return d;
-})
-.attr('opacity','0.75');
+// var circles = svg.selectAll('circle').data(dataset).enter().append('circle');
+
+// circles.attr('cx', function(d, i){
+// 	return (i * 50) + 70;
+// })
+// .attr('cy', h/2)
+// .attr('r', function(d){
+// 	return d;
+// })
+// .attr('opacity','0.75');
